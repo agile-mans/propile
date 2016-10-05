7dff509472bf5f43043dda08c1d0d0a63b1a6740
Serge HARDY <serge.hardy@zenika.com>
2016-10-05 07:36:32 +0200

    Update Le Mans

--- config/application.rb
+++ config/application.rb
@@ -81,11 +81,11 @@ module Propile
     end
 
     def self.mail_subject_prefix
-      "[Agile Tour Rennes 2016]"
+      "[Agile Le Mans 2016]"
     end
 
     def self.mail_sender
-      "cfp.agiletour2016@agilerennes.org"
+      "contact@agile-mans.org"
     end
 
     def self.version
